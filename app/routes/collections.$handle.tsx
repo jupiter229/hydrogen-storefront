f06431531ff8396b404e7fed41e5e41b1ef2d973
jupiter229 <smartwang1004@outlook.com>
2023-06-26 19:45:39 -0700

    feat: add single collection route

---
+++ app/routes/collections.$handle.tsx
@@ -0,0 +1,58 @@
+
+import { LoaderArgs, json } from "@shopify/remix-oxygen";
+import type { Collection as CollectionType } from "@shopify/hydrogen/storefront-api-types";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({params, context}: LoaderArgs ) {
+  const {handle} = params;
+  const {collection} = await context.storefront.query<{
+    collection: CollectionType;
+  }>(COLLECTION_QUERY, {
+    variables: {
+      handle,
+    },
+  });
+
+  // Handle 404s
+  if (!collection) {
+    throw new Response(null, {status: 404});
+  }
+
+  // json is a Remix utility for creating application/json responses
+  // https://remix.run/docs/en/v1/utils/json
+  return json({
+    collection,
+  });
+}
+
+export default function Collection() {
+  const {collection} = useLoaderData() as {collection: CollectionType};
+
+  return (
+    <>
+      <header className="grid w-full gap-4 grid-cols-1 md:grid-cols-2">
+        <h1 className="text-4xl font-bold text-center md:text-left">
+          {collection.title}
+        </h1>
+
+        {collection.description && (
+          <div className="flex items-baseline justify-between w-full">
+            <p className="text-2xl font-bold text-center md:text-left">
+              {collection.description}
+            </p>
+          </div>
+        )}
+      </header>
+    </>
+  )
+}
+
+const COLLECTION_QUERY = `#graphql
+  query CollectionDetails($handle: String!) {
+    collection(handle: $handle) {
+      title
+      description
+      handle
+    }
+  }
+`;
